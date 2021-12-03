c8bf3ba95330b27148a794225468519f2b9e9983
Luba Derebas <luba_d2005@mail.ru>
2021-12-03 19:56:59 +0300

    Start

---
+++ test-vue/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+</template>
+
+<script>
+
+export default {
+  name: 'FirstComponent',
+  data(){
+    return {text: "ewfwf"}
+  }
+}
+</script>
+
+<style>
+.informer-container {
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  width: 400px;
+  min-height: 255px;
+  padding: 30px;
+  background-image: url("../img/forest.png");
+  background-size: cover;
+  background-position: center center;
+  border-radius: 20px;
+  box-shadow: 0px 0px 10px 0px rgba(50, 50, 50, 0.75);
+}
+</style>
